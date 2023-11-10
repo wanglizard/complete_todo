6a208ba93cba1206ec422d1e8f1c286b833d3dbf
wanglizard <cic6307@naver.com>
2023-11-10 10:21:59 +0900

    complete

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,27 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fas fa-plus" aria-hidden="true"></i>
         </span>
+
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">경고</h3>
+            <span slot="footer" @click="showModal = false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
+    props: ['propsdata'],
+
     data() {
         return {
-            newTodoItem: ''
+            newTodoItem: '',
+            showModal: false
         }
     },
 
@@ -19,13 +32,19 @@ export default {
         addTodo() {
             if (this.newTodoItem !== "") {
                 var value = this.newTodoItem && this.newTodoItem.trim();
-                localStorage.setItem(value, value);
+                this.$emit('addTodo', value);
                 this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
             }
         },
 
         clearInput() {
             this.newTodoItem = '';
+        },
+
+        components: {
+            Modal: Modal
         }
     }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,43 +1,45 @@
 <template>
     <section>
-        <ul>
-            <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+        <transition-group name="list" tag="ul">
+            <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow">
                 <i class="checkBtn fas fa-check" aria-hidden="true"></i>
                 {{ todoItem }}
                 <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
                     <i class="far fa-trash-alt" aria-hidden="true"></i>
                 </span>
-
             </li>
-        </ul>
+        </transition-group>
     </section>
 </template>
 
 <script>
 export default {
+    props: ['propsdata'],
+
     data() {
         return {
             todoItems: []
         }
     },
-    created() {
-        if (localStorage.length > 0) {
-            for (var i = 0; i < localStorage.length; i++) {
-                this.todoItems.push(localStorage.key(i));
-            }
-        }
-    },
 
     methods: {
         removeTodo(todoItem, index) {
-            localStorage.removeItem(todoItem);
-            this.todoItems.splice(index, 1);
+            this.$emit('removeTodo', todoItem, index);
         }
     }
 }
 </script>
 
 <style scoped>
+.list-enter-active, .list-leave-active {
+    transition: all 1s;
+}
+
+.list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+}
+
 ul {
     list-style-type: none;
     padding-left: 0px;
